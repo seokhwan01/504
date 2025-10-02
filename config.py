3fa806f6ab285efae2cc9ff91ecbdf071acc1148
seokhwan01 <soho010526@gmail.com>
2025-10-02 18:29:12 +0900

    ip

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = "secret_key_for_session"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MQTT_BROKER = "localhost" #MQTT 브로커 ip로 변경
+    MQTT_BROKER = "192.168.137.1t" #MQTT 브로커 ip로 변경
     MQTT_PORT = 1883
